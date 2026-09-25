a4e157b1cad1965d325e60d2774b9e3559e3d6cb
Rishi Sahu <rishisahu8822@gmail.com>
2026-09-26 00:07:53 +0530

    cors config updated with netlify link

--- src/main/java/com/SMS/demo/CorsConfig.java
+++ src/main/java/com/SMS/demo/CorsConfig.java
@@ -9,7 +9,7 @@ public class CorsConfig implements WebMvcConfigurer {
 
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
-		registry.addMapping("/**").allowedOrigins("http://localhost:5173","https://scintillating-quokka-6a5701.netlify.app/")
+		registry.addMapping("/**").allowedOrigins("http://localhost:5173","https://scintillating-quokka-6a5701.netlify.app/","sms-by-rishi-sahu.netlify.app")
 				.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS").allowedHeaders("*");
 	}
 }
